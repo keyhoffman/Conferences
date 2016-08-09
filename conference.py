49e2b2b38d76cd549492faf23d4f508e4a3e08be
Key Hoffman <key.hoffman@linebounce.net>
2016-08-08 21:02:24 -0500

    fake commit

--- conference.py
+++ conference.py
@@ -248,6 +248,7 @@ class ConferenceApi(remote.Service):
     @endpoints.method(ConferenceQueryForms, ConferenceForms, path='queryConferences', http_method='POST', name='queryConferences')
     def queryConferences(self, request):
         """query for conferences"""
+        # FAKE
         conferences = self._getQuery(request)
 
         # return one individual ConferenceForm object per Conference
