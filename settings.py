1f91791b95a23bd4026d49798692a9d90ff93721
Key Hoffman <key.hoffman@linebounce.net>
2016-08-08 19:15:38 -0500

    Conference_Central initial commit

---
+++ settings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+# Add a new client id for every type of client that needs to be supported. ie IOS_CLIENT_ID = 'someBigFatString'
+WEB_CLIENT_ID = '269708155038-01bbkha61rshqj070jou22q6hs6lqica.apps.googleusercontent.com'
