76cec61f4cf1cf7d4a18add30b02595cbe299f15
Key Hoffman <key.hoffman@linebounce.net>
2016-08-08 19:37:48 -0500

    Test 3 for conference.py

--- conference.py
+++ conference.py
@@ -248,7 +248,8 @@ class ConferenceApi(remote.Service):
     @endpoints.method(ConferenceQueryForms, ConferenceForms, path='queryConferences', http_method='POST', name='queryConferences')
     def queryConferences(self, request):
         """query for conferences"""
-        # pass request
+        # TEST 1
+        # TEST 2
         conferences = self._getQuery(request)
 
         # return one individual ConferenceForm object per Conference
